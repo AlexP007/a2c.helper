18bffbca56b2d8bebbed5bbc5a303c818853f986
AlexP <alex.p.panteleev@gmail.com>
2019-10-23 22:51:42 +0300

    [fix]

--- install/index.php
+++ install/index.php
@@ -15,14 +15,13 @@ Class a2c_helper extends CModule
         $arModuleVersion = array();
         include(__DIR__ . "/version.php");
 
-        $this->MODULE_ID = 'A2C.helper';
+        $this->MODULE_ID = 'a2c.helper';
         $this->MODULE_VERSION = $arModuleVersion["VERSION"];
         $this->MODULE_VERSION_DATE = $arModuleVersion["VERSION_DATE"];
         $this->MODULE_NAME = Loc::getMessage("A2C_HELPER_MODULE_NAME");
-        $this->MODULE_DESCRIPTION = Loc::getMessage("KODIX_AC_MODULE_DESC");
-        $this->PARTNER_NAME = Loc::getMessage("KODIX_AC_PARTNER_NAME");
+        $this->MODULE_DESCRIPTION = Loc::getMessage("A2C_HELPER_MODULE_DESC");
+        $this->PARTNER_NAME = Loc::getMessage("A2C_HELPER_PARTNER_NAME");
         $this->MODULE_SORT = 100;
-
     }
 
     /*

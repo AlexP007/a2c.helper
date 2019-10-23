f926e0f97f2b2a0207087e8bf81509aa80287fb9
AlexP <alex.p.panteleev@gmail.com>
2019-10-23 20:31:57 +0300

    add en lang file

---
+++ lang/en/install.php
@@ -0,0 +1,5 @@
+<?php
+$MESS['A2C_HELPER_MODULE_NAME'] = 'A2C Developer Helper';
+$MESS['A2C_HELPER_MODULE_DESC'] = 'Bundle of useful classes for faster bitrix development ';
+$MESS['A2C_HELPER_PARTNER_NAME'] = 'A2C';
+?>

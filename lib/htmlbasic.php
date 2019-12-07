859661b5605a84920bcc0eadabf120ee61e212a3
AlexP <alex.p.panteleev@gmail.com>
2019-12-07 18:56:21 +0100

    add htmlhelper

---
+++ lib/htmlbasic.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace A2c\Helper;
+
+
+abstract class HtmlBasic
+{
+    private static $voidElements = ['input'];
+
+    protected static function getParams(array $params): array
+    {
+        $attr = $params['ATTR'] ?? [];
+        $innerHtml = $params['INNER_HTML'] ?? '';
+
+        return [$innerHtml, $attr];
+    }
+
+    public static function input(array $params): string
+    {
+        [$innerHtml, $attr] = self::getParams($params);
+
+        return self::renderElement('input', $innerHtml, $attr);
+    }
+
+    public static function renderElement(string $name, string $innerHtml, array $attr): string
+    {
+        return  self::beginTag($name, $attr) . $innerHtml . self::endTag($name);
+    }
+
+    private static function beginTag(string $name, array $attr): string
+    {
+        if ($name === null || $name === false) {
+            return '';
+        }
+
+        return "<$name" . self::renderAttributes($attr);
+    }
+
+    private static function endTag(string $name): string
+    {
+        if (in_array($name, self::$voidElements) ) {
+            return '>';
+        }
+
+        return "</$name>";
+    }
+
+    private static function renderAttributes(array $attr): string
+    {
+        $result = '';
+
+        foreach ($attr as $key => $val) {
+            $attribute = strtolower($key);
+            $value = strtolower($val);
+
+            $result .= "$attribute=\"$value\"";
+        }
+
+        return $result;
+    }
+}
---
+++ lib/htmlhelper.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace A2c\Helper;
+
+class HtmlHelper extends HtmlBasic
+{
+    public static function submit(array $params)
+    {
+        $params['ATTR']['TYPE'] = 'submit';
+
+        return self::input($params);
+    }
+
+    public static function hidden(array $params)
+    {
+        $params['ATTR']['TYPE'] = 'hidden';
+
+        return self::input($params);
+    }
+
+    public static function beginForm(array $params)
+    {
+
+    }
+}
